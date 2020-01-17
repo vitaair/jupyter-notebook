f9e1fdb192f49a002b9a32c83c8050d1cf9d299f
vitaair <vitaair@outlook.com>
2020-01-17 15:15:48 +0800

    PY-2019.3.1 <DELL@DESKTOP-IQRGFN9 Create baseRefactoring.xml, markdown.xml, ide.general.xml, project.default.xml, laf.xml, jdk.table.xml, Default.xml, jupyter-settings.xml, find.xml, ui.lnf.xml, wsl.distributions.xml, filetypes.xml, Python Script.py, vcs.xml, colors.scheme.xml, databaseDrivers.xml, editor.xml, github.xml, debugger.xml, updates.xml

---
+++ fileTemplates/internal/Python Script.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+'''
+=================================================
+@Project -> File   ：${PROJECT_NAME} -> ${NAME}
+@IDE    ：${PRODUCT_NAME}
+@Author ：Mr. Vitaair
+@Site   ：${SITE}
+@Date   ：${DATE} ${TIME}
+@Desc   ：
+==================================================
+@Change Activity   ：${DATE} ${TIME}
+==================================================
+'''
